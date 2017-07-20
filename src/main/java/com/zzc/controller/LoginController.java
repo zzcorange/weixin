757810b228e22e32d9f16c3706ed930db162b4a4
zzcorange <273893361@qq.com>
2017-07-20 16:11:33 +0800

    首次提交

---
+++ src/main/java/com/zzc/controller/LoginController.java
@@ -0,0 +1,13 @@
+package com.zzc.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+*@author zzc
+*@date 2017年7月20日--下午2:13:28
+*/
+@Controller
+public class LoginController {
+	
+}
+
